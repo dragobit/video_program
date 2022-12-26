c4e37bcd4a56ea653820d5a4dabf091d3781d681
dragobit <97995130+dragobit@users.noreply.github.com>
2022-12-26 10:34:19 +0900

    Add Gyao clower

--- script.py
+++ script.py
@@ -1,3 +1,40 @@
 import requests
+import json
+import sys
+from operator import itemgetter
+from pathlib import Path
+import os
 
-print("animeeeeee----")
+def js_open(js):
+    if isinstance(js, str):
+        with open(js,'rt') as f:#, errors='ignore',encoding='unicode-escape'
+            D1 = json.load(f)#object_pairs_hook=OrderedDict 
+    return D1
+
+def js_write(dic, js):
+    if isinstance(js, str):
+        with open(js, 'w') as f:
+            json.dump(dic, f,indent=4, ensure_ascii=False)
+
+res = requests.get("https://gyao.yahoo.co.jp/api/schedule/anime?'")
+d = res.json()
+try:
+    assert(isinstance(d, dict))
+except:
+    sys.exit(1)
+
+key = ["title", "id", "url"]
+title = []
+for day in d['sections']:
+    for item in day['items']:
+        item_d = {k: item[k] for k in key}
+        if item_d not in title:
+            title.append(item_d)
+
+title.sort(key=itemgetter('id'))
+
+data_dir = Path("data", "gyao")
+data_dir = "data/gyao"
+
+js_write(title, os.path.join(data_dir+"title.json")
+
